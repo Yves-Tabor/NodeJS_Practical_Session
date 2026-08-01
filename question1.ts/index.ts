975c51fbcfc17c19a8220052f798b7e743e333e6
Yves-Tabor <walvesoutis@gmail.com>
2026-08-02 01:30:37 +0200

    feat: get the conversion rate

--- question1.ts/index.ts
+++ question1.ts/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import { error } from "node:console";
 
 const app = express();
 const port = 3000;
@@ -10,7 +11,41 @@ const rates: Record<string, number> = {
 };
 
 app.get("/convert", (req, res) => {
+    //Step 1 : Reading data
     const { amount, currency } = req.query;
+    //Step 2: Validation
+    if (amount === undefined || currency === undefined) {
+        return res.json(400).json({
+            error: "Both amount and currency are required",
+        })
+    }
+
+    const validAmount = Number(amount);
+    if (!Number.isFinite(validAmount) || validAmount < 0) {
+        return res.status(400).json({
+            error: "Provide a positive number greater than zero",
+        })
+    }
+
+    if (typeof currency != "string" || !(currency.toLowerCase() in rates)) {
+        return res.status(400).json({
+            error: "Currency must be of usd, eur or gbp",
+        })
+    }
+    // Step 3: Get the conversion rate
+    const rate = rates[currency.toLowerCase()];
+    //Step 4: Performing conversion
+    const convertedAmount = validAmount * rate;
+
+    //Step 5: Sending Response
+    return res.status(200).json({
+        input: {
+            amount: validAmount,
+            currency: currency.toLowerCase(),
+        },
+        convertedAmount,
+        unit: "RWF",
+    })
 
 })
 
