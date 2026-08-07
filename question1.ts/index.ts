0816fce80ccf35a963a375beca0e103c5367e399
Yves-Tabor <walvesoutis@gmail.com>
2026-08-07 21:41:06 +0200

    server configuration

---
+++ question1.ts/index.ts
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+const rates: Record<string, number> = {
+    usd: 1300,
+    eur: 1400,
+    gbp: 1650
+};
+
+app.listen(port, () => {
+    console.log("Server is running on http://localhost:", port);
+});
